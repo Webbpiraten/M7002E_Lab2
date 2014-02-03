105dd6e91630a2c2d7c3ebca3f8f5752241390cf
Webbpiraten <webbpiraten@gmail.com>
2014-02-03 23:45:46 +0100

    Update Shapes.java

--- src/lab2/Shapes.java
+++ src/lab2/Shapes.java
@@ -1,30 +1,90 @@
 package lab2;
 
 import javax.media.opengl.GL2;
+import javax.media.opengl.GLAutoDrawable;
+
+import com.jogamp.opengl.util.gl2.GLUT;
 
 public class Shapes {
 
+    float no_mat[] = 			{ 0.0f, 0.0f, 0.0f, 1.0f };
+    float mat_ambient[] = 		{ 0.7f, 0.7f, 0.7f, 1.0f };
+    float mat_ambient_color[] = { 0.8f, 0.8f, 0.2f, 1.0f };
+    float mat_diffuse[] = 		{ 0.1f, 0.5f, 0.8f, 1.0f };
+    float mat_specular[] = 		{ 1.0f, 1.0f, 1.0f, 1.0f };
+    float no_shininess[] = 		{ 0.0f };
+    float low_shininess[] = 	{ 5.0f };
+    float medium_shininess[] = 	{ 25.0f };
+    float high_shininess[] = 	{ 100.0f };
+    float mat_emission[] =  	{ 0.3f, 0.2f, 0.2f, 0.0f };
 	
-	public Shapes(GL2 gl){
-		Draw_Shape(gl);
+	float xcord,ycord;
+	int shape;
+	GLUT glut = new GLUT();
+	
+    Shapes(float x, float y, int shapes){
+		this.xcord = x;
+		this.ycord = y;
+		shape = shapes;
 	}
-	
-	public void Draw_Shape(GL2 gl){
-		
-		
-		gl.glBegin(GL2.GL_QUADS);
-		gl.glPushAttrib(GL2.GL_CURRENT_BIT);
-			gl.glColor3f(0, 0, 1);
-		gl.glPopAttrib();
-	
-		gl.glPushMatrix();
-			gl.glVertex2f(-0.95f, 0.95f);
-			gl.glVertex2f(-0.85f, 0.95f);
-			gl.glVertex2f(-0.85f, 0.85f);
-			gl.glVertex2f(-0.95f, 0.85f);
-		gl.glPopMatrix();
-	
-	gl.glEnd();
-		
-	}
+    
+    public void SelectShape(GLAutoDrawable drawable){
+    	int toDraw = shape;
+        switch (toDraw) {
+            case 1:  Draw_Teapot(drawable, glut);
+                     break;
+            case 2:  Draw_Sphere(drawable, glut);
+                     break;
+            case 3:  Draw_Cube(drawable, glut);
+                     break;
+            default: System.out.println("Shape doesnt exist!");
+                     break;
+        }
+    }
+
+    public void Draw_Teapot(GLAutoDrawable drawable, GLUT glut){
+    	// Teapot
+    	final GL2 gl = drawable.getGL().getGL2();
+
+    	gl.glPushMatrix();
+    	gl.glTranslatef(xcord, ycord, 0.0f);
+    	gl.glRotatef(15.0f, 1.0f, 1.0f, 0.0f);
+//    	gl.glMaterialfv(GL2.GL_FRONT_AND_BACK, GL2.GL_AMBIENT, no_mat, 0);
+//    	gl.glMaterialfv(GL2.GL_FRONT_AND_BACK, GL2.GL_DIFFUSE, mat_diffuse, 0);
+//    	gl.glMaterialfv(GL2.GL_FRONT_AND_BACK, GL2.GL_SPECULAR, mat_specular, 0);
+//    	gl.glMaterialfv(GL2.GL_FRONT_AND_BACK, GL2.GL_SHININESS, high_shininess, 0);
+//    	gl.glMaterialfv(GL2.GL_FRONT_AND_BACK, GL2.GL_EMISSION, mat_emission, 0);
+    	glut.glutSolidTeapot(0.5f);
+    	gl.glPopMatrix();
+    }
+
+    public void Draw_Sphere(GLAutoDrawable drawable, GLUT glut){
+    	// Sphere
+    	final GL2 gl = drawable.getGL().getGL2();
+
+    	gl.glPushMatrix();
+    	gl.glTranslatef(xcord, ycord, 0.0f);
+//    	gl.glMaterialfv(GL2.GL_FRONT_AND_BACK, GL2.GL_AMBIENT, no_mat, 0);
+//    	gl.glMaterialfv(GL2.GL_FRONT_AND_BACK, GL2.GL_DIFFUSE, mat_diffuse, 0);
+//    	gl.glMaterialfv(GL2.GL_FRONT_AND_BACK, GL2.GL_SPECULAR, mat_specular, 0);
+//    	gl.glMaterialfv(GL2.GL_FRONT_AND_BACK, GL2.GL_SHININESS, low_shininess, 0);
+//    	gl.glMaterialfv(GL2.GL_FRONT_AND_BACK, GL2.GL_EMISSION, no_mat, 0);
+    	glut.glutSolidSphere(0.25f, 200, 200);
+    	gl.glPopMatrix();
+
+    }
+
+    public void Draw_Cube(GLAutoDrawable drawable, GLUT glut){
+    	// Cube
+    	final GL2 gl = drawable.getGL().getGL2();
+    	
+    	gl.glPushMatrix();
+    	gl.glTranslatef(xcord, ycord, 0.0f);
+    	gl.glRotatef(15.0f, 1.0f, 1.0f, 0.0f);
+    	glut.glutSolidCube(0.5f);
+    	gl.glPopMatrix();
+
+    }
 }
+
+
